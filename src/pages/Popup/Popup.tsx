38d52a77f709d1bb06befb76f9d9a45de22b35e5
Zach Godsell <zacharyjamesgodsell@gmail.com>
2023-04-20 18:45:37 +1000

    update

---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Popup.css";
+
+const Popup = () => {
+  return (
+    <div className=" w-[400px] bg-slate-300">
+      <header className="flex flex-wrap">
+        <div className="w-full font-bold text-xl text-center">
+          <p>Afl Tables Export CSV</p>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Popup;
